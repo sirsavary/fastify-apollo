755e8650e84661b61c59388107ab02b594fcd1b4
Nickolena Coop <ncoop@cimro.com>
2017-07-20 09:45:44 -0500

    add example

---
+++ example.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const fastify = require('fastify')()
+const { makeExecutableSchema } = require('graphql-tools')
+
+const typeDefs = `
+type Query {
+    hello: String
+}
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'world'
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+fastify.register(require('./index'), {
+  prefix: '/api',
+  graphql: { schema },
+  graphiql: true,
+  printSchema: true
+})
+
+fastify.listen(8000, function (err) {
+  if (err) {
+    throw err
+  }
+
+  for (let route of fastify) {
+    console.log(route)
+  }
+
+  console.log(`listening on ${fastify.server.address().port}`)
+})
